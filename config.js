44c9c181e7e4d918113b413360a340e3e6926ca8
Xtharuka <121140826+Xtharuka@users.noreply.github.com>
2023-05-18 08:01:09 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['94723357728', 'Tharuka@virus.com', true],
+  ['94723357728', 'Tharuka@virus.com', true], 
   [''] 
 ] //Number of owners
 
